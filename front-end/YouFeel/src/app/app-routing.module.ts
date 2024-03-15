cbb84b15b356707eef9f346a44bf72b20c505bf6
TorresDaCostaTheo <newtete789@gmail.com>
2024-03-15 10:36:17 +0100

    ede

--- front-end/YouFeel/src/app/app-routing.module.ts
+++ front-end/YouFeel/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommentaireComponent } from './commentaire/commentaire.component';
 import { ApiComponent } from './api/api.component';
+import { MockComponent } from './mock/mock.component';
 
 const routes: Routes = [
   { path: 'commentaire', component: CommentaireComponent },
   { path: 'api', component: ApiComponent },
+  { path: 'api', component: MockComponent },
   { path: '', redirectTo: 'api', pathMatch: 'full' },
 ];
 
